853dd700a6ade2954f59c30c60ff7bfd247b394f
Alberto <aghex70@gmail.com>
2020-08-12 02:11:18 +0200

    First admin skeleton

--- jbseekr/apps/seeker/admin.py
+++ jbseekr/apps/seeker/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import (Position, Company, Opinion)
+
+
 # Register your models here.
+class PositionAdmin(admin.ModelAdmin):
+	list_display = (
+		'role', 'minimum_salary', 'maximum_salary', 'posted_date', 'created_date')
+
+
+admin.site.register(Position, PositionAdmin)
