d1ba806447a4e26720d87995e69f632a0966d85e
Alberto <aghex70@gmail.com>
2020-08-13 02:44:42 +0200

    Cleaned code

--- jbseekr/apps/seeker/scraper/base.py
+++ jbseekr/apps/seeker/scraper/base.py
@@ -3,35 +3,18 @@ import requests
 
 class BS4Scraper:
 
-	def __init__(self, source):
+	def __init__(self, source, origin):
 		self.source = source
-		self.url = None
+		self.origin = origin
 		self.content = None
-		# self.title = None
 		self.parser = None
 
-	def retrieve_content(self, url=None):
-		response = requests.get(self.url)
-		# print(f"response ---> {response}")
+	def retrieve_content(self, url):
+		response = requests.get(url)
 		self.content = response.text
-		# print(f"response.text ---> {response.text}")
 		return self.content
 
-	# @property
-	# def title(self):
-	# 	if not self.content:
-	# 		self.retrieve_content()
-	# 	return
-
-	def parse(self):
+	def parse_content(self):
 		if not self.content:
 			self.retrieve_content()
 		self.parser = BeautifulSoup(self.content, "html.parser")
-
-	@staticmethod
-	def search(role, location):
-		pass
-
-
-
-
