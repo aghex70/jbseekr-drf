53df528644accb210d25b77e8051bdcd379f327a
Alberto <aghex70@gmail.com>
2020-08-16 16:35:30 +0200

    Addional fields displayed

--- jbseekr/apps/seeker/admin.py
+++ jbseekr/apps/seeker/admin.py
@@ -6,7 +6,7 @@ from .models import (Position, Company, Opinion)
 # Register your models here.
 class PositionAdmin(admin.ModelAdmin):
 	list_display = (
-		'role', 'minimum_salary', 'maximum_salary', 'posted_date', 'created_date')
+		'role', 'minimum_salary', 'maximum_salary', 'posted_date', 'created_date', 'source', 'hidden', 'highlighted')
 
 
 admin.site.register(Position, PositionAdmin)
