3d9bd0203eb563c44c5d387d83e97221c16bf35e
Alberto <aghex70@gmail.com>
2020-08-16 04:10:30 +0200

    Set default values if serializer is not filled

--- jbseekr/apps/seeker/serializers.py
+++ jbseekr/apps/seeker/serializers.py
@@ -8,5 +8,13 @@ class PositionSerializer(serializers.ModelSerializer):
         #fields = ('pk', 'name', 'email', 'document', 'phone', 'registrationDate')
 
 class PositionQuerySerializer(serializers.Serializer):
-    role = serializers.CharField(default="Python", required=False)
-    location = serializers.CharField(default="Madrid", required=False)
+    role = serializers.CharField(default="Python")
+    location = serializers.CharField(default="Madrid")
+
+    def save(self):
+        position = {
+            "role": "Python" if self.validated_data['role'] == "string" else self.validated_data['role'],
+            "location": "Madrid" if self.validated_data['location'] == "string" else self.validated_data['location'],
+        }
+
+        return position
