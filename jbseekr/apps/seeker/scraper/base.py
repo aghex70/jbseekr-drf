ff05a6df74401589321a889cfddb09a759c1a19c
Alberto <aghex70@gmail.com>
2020-08-18 01:12:55 +0200

    Added requests-html parser

--- jbseekr/apps/seeker/scraper/base.py
+++ jbseekr/apps/seeker/scraper/base.py
@@ -1,4 +1,5 @@
 import requests
+from requests_html import HTMLSession
 
 from bs4 import BeautifulSoup
 
@@ -18,5 +19,17 @@ class BS4Scraper:
 
 	def parse_content(self):
 		if not self.content:
-			self.retrieve_content()
+			self.retrieve_content(self.origin)
 		self.parser = BeautifulSoup(self.content, "html.parser")
+
+
+class RequestsHTMLParser:
+	def __init__(self, source, origin):
+		self.source = source
+		self.origin = origin
+		self.session = HTMLSession()
+		self.content = None
+		self.positions = []
+
+	def fetch_url(self, url):
+		self.content = self.session.get(url)
